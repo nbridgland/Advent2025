bfcc3f507bdd708647d8e8fa301dda416003e4eb
nbridgland <nbridgland@gmail.com>
2025-12-10 17:48:45 -0500

    day 8 solutions

---
+++ day08_2.py
@@ -0,0 +1,53 @@
+def find_distance(box_coordinate_1, box_coordinate_2):
+    return ((box_coordinate_1[0] - box_coordinate_2[0])**2 + (box_coordinate_1[1] - box_coordinate_2[1])**2 + (box_coordinate_1[2] - box_coordinate_2[2])**2)**(1/2)
+
+def combine_common_verticies(vertex_lists):
+    i = 0
+    while i < len(vertex_lists):
+        j = i + 1
+        while j < len(vertex_lists):
+            for vertex in vertex_lists[i]:
+                if vertex in vertex_lists[j]:
+                    for vertex_0 in vertex_lists[j]:
+                        if vertex_0 not in vertex_lists[i]:
+                            vertex_lists[i].append(vertex_0)
+                    vertex_lists.pop(j)
+                    j = i
+                    break
+            j += 1
+        i += 1
+
+if __name__ == "__main__":
+    with open("day08_input.txt") as f:
+        data = f.read()
+    box_coordinates = [[int(coordinate) for coordinate in entry.split(',')] for entry in data.split('\n')]
+    distance_dictionary = {}
+    for i in range(len(box_coordinates)):
+        for j in range(i+1, len(box_coordinates)):
+            distance = find_distance(box_coordinates[i], box_coordinates[j])
+            if distance not in distance_dictionary:
+                distance_dictionary[distance] = [(i, j)]
+            else:
+                print('This edge case occurred!') # it did not
+                distance_dictionary[distance].append((i, j))
+    vertex1, vertex2 = distance_dictionary[min(distance_dictionary.keys())][0]
+    distance_dictionary.pop(min(distance_dictionary.keys()))
+    connected_verticies = [[vertex1, vertex2]]
+    while len(connected_verticies[0]) < len(box_coordinates):
+        vertex1, vertex2 = distance_dictionary[min(distance_dictionary.keys())][0]
+        distance_dictionary.pop(min(distance_dictionary.keys()))
+        added = False
+        for vertex_list in connected_verticies:
+            if vertex1 in vertex_list or vertex2 in vertex_list:
+                added = True
+                if vertex1 not in vertex_list:
+                    vertex_list.append(vertex1)
+                    combine_common_verticies(connected_verticies)
+                if vertex2 not in vertex_list:
+                    vertex_list.append(vertex2)
+                    combine_common_verticies(connected_verticies)
+        if not added:
+            connected_verticies.append([vertex1, vertex2])
+    print(f"{len(connected_verticies[0])} of {len(box_coordinates)} boxes connected")
+    print(box_coordinates[vertex1], box_coordinates[vertex2])
+    print(box_coordinates[vertex1][0]*box_coordinates[vertex2][0])
